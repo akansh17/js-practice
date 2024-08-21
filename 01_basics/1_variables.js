7da02c488c44fad414ceb3d2c2a61b135a3bff53
akansh17 <56785300+akansh17@users.noreply.github.com>
2024-08-21 18:59:18 +0000

    initial commit

---
+++ 01_basics/1_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "akansh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Chandigarh"
+
+// accountId = 2
+
+accountEmail = "aggarwal@gmail.com"
+accountPassword = "98765"
+accountCity = "Panchkula"
+
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" //Treat all JS code as the newer version
+alert(3+3)
+
+// ---------Summary---------
+
+// Primitive (call by value)
+// String, Number, Bool, null, undefined, Symbol, BigInt
+
+// Non-Primitive/Reference Types
+// Array, Objects, Functions
+
+const heros = ["Ironman","spiderman"] //Array
+
+let myObj = {
+    name: "Akansh",
+    age: 26,
+} //Object
+
